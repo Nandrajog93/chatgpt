b63652edfdfc93850da7df96c4dc60f1be7a0620
Nandrajog93 <nandrajog.aakash@yahoo.com>
2023-04-02 12:55:57 +0200

    commit new changes

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
+import 'dart:developer';
 import 'dart:io';
-
+import 'package:chatgpt/models/models_model.dart';
 import 'package:http/http.dart' as http;
 
 import '../constant/api_consts.dart';
 
 
 class ApiService{
-  static Future <void> getModels() async {
+  static Future <List<ModelsModel>> getModels() async {
 
     try{
      var response = await http.get(Uri.parse("$BASE_URL/models"),
@@ -21,8 +22,23 @@ class ApiService{
         throw HttpException(jsonResponse['error']['message']);
       }
       print("jsonResponse $jsonResponse");
+
+      List temp =[];
+      for (var value in jsonResponse['data']){
+        //print(value["id"]);
+        temp.add(value);
+        log("temp ${value["id"]}");
+  
+        //print(1.toString());
+        
+        }
+
+
+      return  ModelsModel.modelsFromSnapshot(temp);
     } catch(error) {
-      print("error $error");
+      
+      log("temp_error $error");
+      rethrow;
     }
 
 
--- lib/widgets/drop_down.dart
+++ lib/widgets/drop_down.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../constant/text_widget.dart';
+import '../models/models_model.dart';
+import '../services/api_services.dart';
+
 class DropDown extends StatefulWidget {
   const DropDown({super.key});
 
@@ -11,18 +15,42 @@ class DropDown extends StatefulWidget {
 }
 
 class _DropDownState extends State<DropDown> {
-  String currentModel ="Model1";
+  String currentModel ="text-davinci-003";
   @override
   Widget build(BuildContext context) {
-      return DropdownButton(
-        dropdownColor: scaffoldBackgroundColor,
-        iconEnabledColor: Colors.white,
-      items: getModelsItem,
-      value: currentModel,
-      onChanged: (value) => {setState(() {
-        currentModel =value.toString();
-      })
-      },
-      );
+      return FutureBuilder<List<ModelsModel>>(
+        
+        future: ApiService.getModels(),
+        builder: (context,snapshot){
+          if (snapshot.hasError){
+              return Center(
+                //int.parse(snapshot.data.data['totalPg'].toString());
+                child: TextWidget(label:snapshot.error.toString()),
+                );
+          }
+         return snapshot.data  == null || snapshot.data!.isEmpty 
+         ? const SizedBox.shrink() 
+         :  FittedBox(
+           child: DropdownButton(
+                 dropdownColor: scaffoldBackgroundColor,
+                 iconEnabledColor: Colors.white,
+               items: List<DropdownMenuItem<String>>.generate(snapshot.data!.length, 
+               
+           (index) => DropdownMenuItem(
+             value:snapshot.data![index].id,
+             
+             child: TextWidget(
+             label: snapshot.data![index].id,
+             fontSize: 15, ))),
+               value: currentModel,
+               onChanged: (value) => {setState(() {
+                 currentModel =value.toString();
+               })
+               },
+               ),
+         );
+
+        });
   }
 }
+
